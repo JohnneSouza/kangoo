d6b85e63bba6340ac72e505a148dfeb2f61dd71e
Johnnes Souza <jhonnesf5@gmail.com>
2024-02-19 23:37:09 -0300

    feat - Include spring-boot-starter-security

--- customers/src/main/java/dev/kangoo/com/customers/service/CustomerService.java
+++ customers/src/main/java/dev/kangoo/com/customers/service/CustomerService.java
@@ -5,25 +5,27 @@ import dev.kangoo.com.customers.domain.entity.CustomerEntityBuilder;
 import dev.kangoo.com.customers.domain.request.CustomerRequest;
 import dev.kangoo.com.customers.domain.response.CustomerResponse;
 import dev.kangoo.com.customers.repository.CustomerRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.net.PasswordAuthentication;
+import java.security.SecureRandom;
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder(10, new SecureRandom());
     }
 
     public CustomerResponse saveCustomer(CustomerRequest customerRequest) {
         //TODO: Verify if the user already exists (Conflict - 409)
 
         CustomerEntity entity = CustomerEntityBuilder.fromCustomerRequest(customerRequest);
-        //TODO: HASH the Password before persisting
-        entity.setPasswordHash(customerRequest.getPassword());
+        entity.setPasswordHash(this.passwordEncoder.encode(customerRequest.getPassword()));
         CustomerEntity saved = this.customerRepository.save(entity);
 
         return CustomerEntityBuilder.toCustomerResponse(saved);
