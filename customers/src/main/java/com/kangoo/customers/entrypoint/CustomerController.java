7db19dd9fa08139d0bdbe0f88ac1614e217d14a6
Johnnes Souza <jhonnesf5@gmail.com>
2024-09-29 01:35:01 -0300

    feat - Add code generation

--- customers/src/main/java/com/kangoo/customers/entrypoint/CustomerController.java
+++ customers/src/main/java/com/kangoo/customers/entrypoint/CustomerController.java
@@ -1,8 +1,23 @@
 package com.kangoo.customers.entrypoint;
 
+import com.kangoo.customers.domain.graphql.CustomerDTO;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
+
 
 @Controller
 public class CustomerController {
+
+    @QueryMapping
+    public Mono<CustomerDTO> findById(@Argument String id){
+        return Mono.just(new CustomerDTO.Builder()
+                .setId(id)
+                .setEmail("email@provider.com")
+                .setFirstName("First Name")
+                .setLastName("Last Name")
+                .build());
+    }
 
 }
