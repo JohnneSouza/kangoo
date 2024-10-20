75af3f3870947e75fcb107bbf5233951a7709e4c
Johnnes Souza <jhonnesf5@gmail.com>
2024-10-20 19:27:51 -0300

    Add tests for Mappers

--- backend/products/src/test/java/dev/kangoo/mappers/ProductMapperImplTest.java
+++ backend/products/src/test/java/dev/kangoo/mappers/ProductMapperImplTest.java
@@ -1,10 +1,14 @@
 package dev.kangoo.mappers;
 
 import dev.kangoo.domain.product.ProductEntity;
+import dev.kangoo.domain.product.ProductEntityMock;
 import dev.kangoo.domain.product.ProductRequest;
+import dev.kangoo.domain.product.ProductRequestMock;
 import dev.kangoo.domain.product.ProductResponse;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ProductMapperImplTest {
@@ -35,5 +39,41 @@ class ProductMapperImplTest {
         assertNull(entity);
 
     }
+
+    @Test
+    public void shouldMapToEntityCorrectlyWhenGivenProductRequestMock() {
+        // Given
+        ProductRequest request = ProductRequestMock.withAllFields();
+
+        // When
+        ProductEntity entity = this.mapper.mapToEntity(request);
+
+        // Then
+        assertNotNull(entity);
+        assertEquals(request.getName(), entity.getName());
+        assertEquals(request.getDescription(), entity.getDescription());
+        assertEquals(request.getPrice(), entity.getPrice());
+        assertEquals(request.getImageUrl(), entity.getImageUrl());
+        assertEquals(request.getCategory(), entity.getCategory());
+    }
+
+    @Test
+    public void shouldMapToResponseCorrectlyWhenGivenProductEntityMock() {
+        // Given
+        ProductEntity entity = ProductEntityMock.withAllFields();
+
+        // When
+        ProductResponse response = this.mapper.mapToResponse(entity);
+
+        // Then
+        assertNotNull(response);
+        assertEquals(entity.getId().toString(), response.getId());
+        assertEquals(entity.getName(), response.getName());
+        assertEquals(entity.getDescription(), response.getDescription());
+        assertEquals(entity.getPrice(), response.getPrice());
+        assertEquals(entity.getImageUrl(), response.getImageUrl());
+        assertEquals(entity.getCategory(), response.getCategory());
+    }
+
 
 }
