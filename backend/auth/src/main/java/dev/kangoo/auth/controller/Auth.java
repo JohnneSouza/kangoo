ec2a2529520de9216f25e7c23109df928ad0cdcc
Johnnes Souza <jhonnesf5@gmail.com>
2025-03-18 20:59:58 -0300

    Rename Login controller

--- backend/auth/src/main/java/dev/kangoo/auth/controller/Auth.java
+++ backend/auth/src/main/java/dev/kangoo/auth/controller/Auth.java
@@ -5,18 +5,23 @@ import dev.kangoo.auth.domain.request.CustomerRequest;
 import dev.kangoo.auth.domain.response.AuthResponse;
 import dev.kangoo.auth.domain.response.CustomerResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public interface Login {
+public interface Auth {
 
+    @PostMapping("login")
     @ResponseStatus(HttpStatus.OK)
     AuthResponse login(@RequestBody AuthRequest authRequest);
 
+    @PostMapping("signup")
     @ResponseStatus(HttpStatus.CREATED)
     CustomerResponse signup(@RequestBody CustomerRequest customerRequest);
 
+    @GetMapping("activation/{code}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void activeCustomerAccount(@PathVariable String code);
 
--- backend/auth/src/main/java/dev/kangoo/auth/controller/AuthController.java
+++ backend/auth/src/main/java/dev/kangoo/auth/controller/AuthController.java
@@ -1,41 +1,36 @@
 package dev.kangoo.auth.controller;
 
-import dev.kangoo.auth.services.CustomersService;
 import dev.kangoo.auth.domain.request.AuthRequest;
 import dev.kangoo.auth.domain.request.CustomerRequest;
 import dev.kangoo.auth.domain.response.AuthResponse;
 import dev.kangoo.auth.domain.response.CustomerResponse;
+import dev.kangoo.auth.services.CustomersService;
 import dev.kangoo.auth.services.authentication.AuthenticationService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/auth")
-public class LoginController implements Login {
+public class AuthController implements Auth {
 
     private final AuthenticationService authenticationService;
     private final CustomersService customersService;
 
-    public LoginController(AuthenticationService authenticationService, CustomersService customersService) {
+    public AuthController(AuthenticationService authenticationService, CustomersService customersService) {
         this.authenticationService = authenticationService;
         this.customersService = customersService;
     }
 
-    @PostMapping("login")
     public AuthResponse login(@RequestBody AuthRequest authRequest) {
         return this.authenticationService.generateToken(authRequest);
     }
 
-    @PostMapping("signup")
     public CustomerResponse signup(@RequestBody CustomerRequest customerRequest) {
         return this.customersService.customerSignUp(customerRequest);
     }
 
-    @GetMapping("activation/{code}")
     public void activeCustomerAccount(@PathVariable String code) {
         this.customersService.activateAccount(code);
     }
