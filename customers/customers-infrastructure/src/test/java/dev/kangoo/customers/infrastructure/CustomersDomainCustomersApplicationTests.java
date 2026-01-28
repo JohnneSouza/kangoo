a297da625ccfb0bad010abc2533849b08bcd91d1
Johnnes Souza <jhonnesf5@gmail.com>
2026-01-28 01:31:06 -0300

    [feat] Refactor customer-service to apply clean arch with maven modules

---
+++ customers/customers-infrastructure/src/test/java/dev/kangoo/customers/infrastructure/CustomersDomainCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kangoo.customers.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersDomainCustomersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
